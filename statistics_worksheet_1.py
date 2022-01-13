419ef88c9d01c16ee9881614fe49782a20565052
rajaravichandkandavalli <89854154+rajaravichandkandavalli@users.noreply.github.com>
2022-01-13 15:55:24 +0530

    Add files via upload

---
+++ statistics_worksheet_1.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 1. Bernoulli random variables take(only) the values 1 and 0
+
+
+# In[2]:
+
+
+# a) True
+
+
+# In[3]:
+
+
+# 2.Which of the following theorem states that the distribution of averages of iid variables, properly normalized, 
+#becomes that of a standard normal as the sample size increases?
+
+
+# In[4]:
+
+
+# a) Central limit therorem
+
+
+# In[5]:
+
+
+# 3. Which of the following is incorrect with respect to use of Poisson distribution?
+
+
+# In[6]:
+
+
+# b)Modelling bounded count data
+
+
+# In[7]:
+
+
+# 4. point out the correct statement.
+
+
+# In[8]:
+
+
+# a) The exponent of a normally distributed random variables follows what is called the log-normal distribution
+
+
+# In[9]:
+
+
+# b) Sums of normally distributed random variables are again normally distributed
+#  even if the variables are dependent
+
+
+# In[10]:
+
+
+# c) The square of a standard normal random variable follows what is called chi-squared distribution
+
+
+# In[11]:
+
+
+# above all are correct
+
+
+# In[12]:
+
+
+# 5. _________ random variables are used to model rates.
+
+
+# In[13]:
+
+
+# c)Poisson
+
+
+# In[14]:
+
+
+# 6. usaually replacing the standard error by its estimated value does not change the CLT
+# a) True
+
+
+# In[15]:
+
+
+# 7.Which of the following testing is concerned with making decisions using data
+# b) Hypothesis
+
+
+# In[16]:
+
+
+# 8.Normalized data are centered at __ and have units equal to standard deviations of the original data.
+
+
+# In[17]:
+
+
+# c)1
+
+
+# In[18]:
+
+
+# 9.Which of the following statement is incorrectwith rescept to outliers?
+
+
+# In[ ]:
+
+
+# c)Outliers cannot comform to the regression relationship
+
